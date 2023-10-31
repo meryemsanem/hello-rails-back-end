7c8eca2788d157ef1b250da029c5e19101d2193c
meryemsanem <meryemsanemkucukbayram@gmail.com>
2023-10-31 20:45:02 +0300

    Update routes

--- app/controllers/api/v1/greetings_controller.rb
+++ app/controllers/api/v1/greetings_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::GreetingsController < ApplicationController
-  def random_greeting
-    @random_greeting = Greeting.order('RANDOM()').first
-    render json: { greeting: @random_greeting.message }
+  def index
+    @greetings = Greeting.all
+    render json: @greetings, status: :ok
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,3 @@
 Rails.application.routes.draw do
-  namespace :api do
-    namespace :v1 do
-      get 'random_greeting', to: 'greetings#random_greeting'
-    end
-  end
+  root to: "api/v1/greetings#index"
 end
